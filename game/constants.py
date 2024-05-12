de0ffa6d83ef28e3c7ad162b3a99b0b18ced41cb
abdallhfattah <abdallh.fattah33@gmail.com>
2024-05-12 22:42:44 +0300

    get coordinate & direction

--- game/constants.py
+++ game/constants.py
@@ -1,16 +1,38 @@
+# dimensions
 WIDTH, HEIGHT = 800, 800
 ROWS, COLS = 8, 8
 SQUARE_SIZE = WIDTH // COLS
 
 # rgb
 RED = (255, 0, 0)
+
 WHITE = (255, 255, 255)
+
 BLACK = (0, 0, 0)
+
 BLUE = (0, 0, 255)
+
 GREY = (128, 128, 128)
-GREEN = (0, 255, 0)
+
+GREEN = (0, 188, 140)
+
+# directions
+DIRECTIONS = [[0, 1], [0, -1], [1, 0], [-1, 0]]
+
+
+# Render
+FPS = 60
+
+# Utility function 
+def get_coordinate_mouse(pos):
+    x, y = pos
+    row = y // SQUARE_SIZE
+    col = x // SQUARE_SIZE
+    return row, col
+
 
 PADDING = 15
 OUTLINE = 2
 
-DEMO_SEARCH_DEPTH = 5  # ai level
+# ai level
+SEARCH_DEPTH = 5  
