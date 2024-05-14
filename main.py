4abf1d2181c9402c53bf0535cbea546b7cc4d60e
Omar <omarkhaled1752003@yahoo.com>
2024-05-14 14:10:10 +0300

    fixed a bug where the game ended before the last move was played

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import pygame
 from game.board import Board
 from game.constants import (FPS, HEIGHT, EASY, MEDIUM, HARD, WHITE, WIDTH,
                             get_coordinate_mouse)
-from game.controller import Game
+from game.controller import GameController
 from minimax.minimaxAlgo import minimax
 
 WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
@@ -14,7 +14,7 @@ pygame.display.set_caption('Othello')
 def main():
     run = True
     clock = pygame.time.Clock()
-    game = Game(WINDOW)
+    game = GameController(WINDOW)
     s = 1
     while run:
         clock.tick(FPS)
@@ -25,14 +25,15 @@ def main():
                 game.change_turn()
                 continue
 
-            value, new_board = minimax(game.get_board(), HARD, WHITE)
+            value, new_board = minimax(game.get_board(), HARD, float('-inf'), float('inf'), WHITE)
             game.ai_move(new_board)
+            game.update()
 
         if game.winner() != None:
             print(game.winner())
             # display the winner in GUI
             run = False
-            pygame.time.delay(10000)
+            pygame.time.delay(5000)
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -42,6 +43,7 @@ def main():
                 pos = pygame.mouse.get_pos()
                 row, col = get_coordinate_mouse(pos)
                 game.select(row, col)
+                game.update()
 
         game.update()
 
